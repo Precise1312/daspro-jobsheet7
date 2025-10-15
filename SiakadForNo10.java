207fc8d8f627734b752fba4e5982ab55ccf26b20
Nama Kamu <yayohajid@gmail.com>
2025-10-15 11:20:12 +0700

    update2

--- SiakadForNo10.java
+++ SiakadForNo10.java
@@ -1,19 +1,29 @@
-import java.util.Scanner;
+    import java.util.Scanner;
 
-public class SiakadForNo10 {
-public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-double nilai, tertinggi = 0, terendah = 100;
-for (int i = 1; i <=10; i++){
-    System.out.print("Masukkan nilai mahasiswa ke -" + i + "; ");
-    nilai = sc.nextDouble();
-    if (nilai > tertinggi){
-        tertinggi = nilai;
+    public class SiakadForNo10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    double nilai, tertinggi = 0, terendah = 100;
+    int lulus = 0;
+    int tidakLulus = 0;
+    for (int i = 1; i <=10; i++){
+        System.out.print("Masukkan nilai mahasiswa ke -" + i + "; ");
+        nilai = sc.nextDouble();
+        if (nilai > tertinggi){
+            tertinggi = nilai;
+        }
+        if (nilai < terendah){
+            terendah = nilai;
+        }
+        if (nilai > 60){
+            lulus = lulus +1;
+        }
+        else {
+            tidakLulus = tidakLulus + 1;
+        }
     }
-    if (nilai < terendah){
-        terendah = nilai;
-    }
-}
-System.out.println("Nilai tertinggi: " + tertinggi);
-System.out.println("Nilai terendah: "+ terendah);
-}}
+    System.out.println("Nilai tertinggi: " + tertinggi);
+    System.out.println("Nilai terendah: "+ terendah);
+    System.out.println("Jumlah Mahasiswa Lulus: "+ lulus);
+    System.out.println("Jumlah Mahasiswa Tidak Lulus: "+ tidakLulus);
+    }}
